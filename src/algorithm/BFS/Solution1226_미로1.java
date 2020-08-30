1e1cde8405fe0862c4a8bf0744436c747e72b32b
박상우 <70404643+upswp@users.noreply.github.com>
2020-08-31 02:58:21 +0900

    nPr,nCr,subset 완료 // BFS 시작

---
+++ src/algorithm/BFS/Solution1226_미로1.java
@@ -0,0 +1,10 @@
+package algorithm.BFS;
+
+public class Solution1226_미로1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/algorithm/subset/basic/Basic_SubSet.java
+++ src/algorithm/subset/basic/Basic_SubSet.java
@@ -5,53 +5,65 @@ import java.io.InputStreamReader;
 import java.lang.Character.Subset;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 /**
  * Basic SubSet
+ * 부분집합의 갯수가 입력된 Sum의 값이 일치하는 경우를 카운트.
  * 
  * @author Park Sangwoo
  * @since 2020-08-30
  */
 public class Basic_SubSet {
-	static int N , totalCnt;
+	static int N, Sum, totalCnt;
 	static int[] input;
 	static boolean[] isSelected;
-	
+
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
-		StringTokenizer st = null;
-		N = Integer.parseInt(br.readLine());
-		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		Sum = Integer.parseInt(st.nextToken()); // 목표 합
 		input = new int[N];
 		isSelected = new boolean[N];
-		
+
 		st = new StringTokenizer(br.readLine().trim());
 		for (int i = 0; i < N; i++) {
 			input[i] = Integer.parseInt(st.nextToken());
 		}
 		SubSet(0);
-		System.out.println("총 경우의 수: "+ totalCnt);
-		
+		System.out.println("총 경우의 수: " + totalCnt);
+
 	}
+
 	public static void SubSet(int cnt) {
 		if (cnt == N) {
-			++totalCnt;
+			// 부분 집합의 구성요소의 합
+			int sum = 0;
 			for (int i = 0; i < N; i++) {
-				System.out.print(isSelected[i]?input[i]:"X");
-				System.out.print("\t");
+				if (isSelected[i]) {
+					sum += input[i];
+				}
 			}
-			System.out.println();
+			if (sum == Sum) {
+				++totalCnt;
+				for (int i = 0; i < N; i++) {
+					System.out.print(isSelected[i] ? input[i] : "X");
+					System.out.print("\t");
+				}
+				System.out.println();
+			}
 			return;
 		}
-		
-		//부분집합 구성에 포함
+
+		// 부분집합 구성에 포함
 		isSelected[cnt] = true;
-		//다음원소로 넘어감
-		SubSet(cnt+1);
-		//부분집합 구성에 비포함
+		// 다음원소로 넘어감
+		SubSet(cnt + 1);
+		// 부분집합 구성에 비포함
 		isSelected[cnt] = false;
-		//다음 원소로 넘어감
-		SubSet(cnt+1);
-		
+		// 다음 원소로 넘어감
+		SubSet(cnt + 1);
+
 	}
 
 }
